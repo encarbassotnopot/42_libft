08631c152436ddad8cad09a60378fec689994007
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-06-19 18:34:12 +0200

    pel cul

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 15:21:00 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/06/19 16:39:56 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/06/19 18:33:55 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,16 +39,13 @@ static unsigned int	get_split_count(char const *s, char c)
 	return (count);
 }
 
-static unsigned int	**get_split_ends(char const *s, char c, unsigned int count)
+static void	pop_split_starts(char const *s, char c,
+		unsigned int *out)
 {
 	unsigned int	i;
 	unsigned int	in_delim;
 	unsigned int	found;
-	unsigned int	**out;
 
-	out = malloc(sizeof(void *) * 2);
-	out[0] = malloc(sizeof(void *) * count);
-	out[1] = malloc(sizeof(void *) * count);
 	i = 0;
 	in_delim = 1;
 	found = 0;
@@ -59,18 +56,49 @@ static unsigned int	**get_split_ends(char const *s, char c, unsigned int count)
 			if (in_delim)
 			{
 				in_delim = 0;
-				out[0][found] = i;
+				out[found++] = i;
 			}
 		}
+		else
+			in_delim = 1;
+		i++;
+	}
+}
+
+static void	pop_split_ends(char const *s, char c, unsigned int count,
+		unsigned int *out)
+{
+	unsigned int	i;
+	unsigned int	in_delim;
+	unsigned int	found;
+
+	i = 0;
+	in_delim = 1;
+	found = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+			in_delim = 0;
 		else if (!in_delim)
 		{
 			in_delim = 1;
-			out[1][found++] = i;
+			out[found++] = i;
 		}
 		i++;
 	}
 	if (found != count)
-		out[1][found] = i;
+		out[found] = i;
+}
+
+static unsigned int	**get_split_ends(char const *s, char c, unsigned int count)
+{
+	unsigned int	**out;
+
+	out = malloc(sizeof(void *) * 2);
+	out[0] = malloc(sizeof(void *) * count);
+	out[1] = malloc(sizeof(void *) * count);
+	pop_split_starts(s, c, out[0]);
+	pop_split_ends(s, c, count, out[1]);
 	return (out);
 }
 
