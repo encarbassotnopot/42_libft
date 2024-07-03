4e01d0953524b08f19b824693fa4552ce61cd723
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-07-03 13:29:31 +0200

    implementat atoi_fwd

---
+++ ft_raw_atoi_fwd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_raw_atoi_fwd.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 18:06:37 by ecoma-ba          #+#    #+#             */
+/*   Updated: 2024/07/03 13:29:04 by ecoma-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stddef.h>
+
+unsigned int	ft_raw_atoi_fwd(char *str)
+{
+	unsigned int	result;
+
+	if (!str)
+		return (0);
+	result = 0;
+	while (*str >= '0' && *str <= '9')
+	{
+		result *= 10;
+		result += *str - '0';
+		str++;
+	}
+	return (result);
+}
