6fc91ab53b1bc1f1ffda40669e2070029b297920
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-07-03 13:33:11 +0200

    oopsie

--- ft_raw_atoi_fwd.c
+++ ft_raw_atoi_fwd.c
@@ -6,23 +6,23 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 18:06:37 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/07/03 13:29:04 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/07/03 13:32:55 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
 
-unsigned int	ft_raw_atoi_fwd(char *str)
+unsigned int	ft_raw_atoi_fwd(char **str)
 {
 	unsigned int	result;
 
 	if (!str)
 		return (0);
 	result = 0;
-	while (*str >= '0' && *str <= '9')
+	while (**str >= '0' && **str <= '9')
 	{
 		result *= 10;
-		result += *str - '0';
-		str++;
+		result += **str - '0';
+		*str += 1;
 	}
 	return (result);
 }
