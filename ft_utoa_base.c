a97e5376a95903e56b9655b8c0056832663491ae
Eina <109956262+encarbassotnopot@users.noreply.github.com>
2024-07-06 11:12:54 +0200

    bug arreglat

--- ft_utoa_base.c
+++ ft_utoa_base.c
@@ -6,19 +6,18 @@
 /*   By: ecoma-ba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 15:11:38 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/07/06 10:39:11 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/07/06 11:11:54 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
-static size_t	get_str_size(unsigned int n, unsigned int radix)
+static int	get_str_size(unsigned int n, unsigned int radix)
 {
-	size_t	len;
+	int	len;
 
-	len = 2;
-	while (n >= radix)
+	len = 1;
+	while (n > radix)
 	{
 		n /= radix;
 		len++;
@@ -30,14 +29,14 @@ char	*ft_utoa_base(unsigned int n, const char *base)
 {
 	char	*out;
 	size_t	radix;
-	size_t	len;
+	int		len;
 
 	radix = ft_strlen(base);
 	len = get_str_size(n, radix);
-	out = ft_calloc(len, sizeof(char));
+	out = ft_calloc(len+1, sizeof(char));
 	if (!out)
 		return (NULL);
-	while (n != 0)
+	while (len > 0)
 	{
 		out[--len] = base[n % radix];
 		n /= radix;
