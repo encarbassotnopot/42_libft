4457a662efc45d28f6e31696aa1bb23e69afe29f
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-07-08 17:24:09 +0200

    errors calculant tamany de base

--- ft_utoa_base.c
+++ ft_utoa_base.c
@@ -6,7 +6,7 @@
 /*   By: ecoma-ba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 15:11:38 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/07/06 11:16:30 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/07/08 17:12:57 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	get_str_size(unsigned int n, unsigned int radix)
 	int	len;
 
 	len = 1;
-	while (n > radix)
+	while (n >= radix)
 	{
 		n /= radix;
 		len++;
